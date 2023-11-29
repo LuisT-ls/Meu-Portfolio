34b27c0b524a01ce62af9d2f1b2ceae283e6621d
Luís Teixeira <84951329+LuisT-ls@users.noreply.github.com>
2023-11-29 13:44:52 +0000

    Adição de descrições

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 ;(() => {
+  // Seleciona o botão do menu e o elemento de navegação
   const menuBtn = document.querySelector('.menu-btn')
   const nav = document.querySelector('nav')
 
+  // Função para alternar a exibição do menu
   const toggleMenu = () => {
     nav.classList.toggle('show')
   }
 
+  // Adiciona um ouvinte de evento de clique ao botão do menu
   menuBtn.addEventListener('click', toggleMenu)
 })()
