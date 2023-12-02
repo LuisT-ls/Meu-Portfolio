6323d607ca3229ee101b7599519763d70666eca0
Luís Teixeira <84951329+LuisT-ls@users.noreply.github.com>
2023-12-02 01:42:13 +0000

    Implementação das skills em cartões

--- script.js
+++ script.js
@@ -11,3 +11,23 @@
   // Adiciona um ouvinte de evento de clique ao botão do menu
   menuBtn.addEventListener('click', toggleMenu)
 })()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const progressBars = document.querySelectorAll('.skill-progress')
+
+  progressBars.forEach(bar => {
+    const value = bar.getAttribute('data-progress')
+    bar.style.width = value
+  })
+})
+
+progressBars.forEach(bar => {
+  let progress = 0
+
+  const interval = setInterval(() => {
+    bar.style.width = progress + '%'
+    progress++
+
+    if (progress > value) clearInterval(interval)
+  }, 10)
+})
