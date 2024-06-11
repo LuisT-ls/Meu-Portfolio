8adc56a8c7f9ab2090b58624aa70e9ef969a7bd1
LuisT-ls <luishg213@outlook.com>
2024-06-10 22:11:46 -0300

    tentativa 4

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
-const fs = require('fs-extra')
+import fs from 'fs-extra';
 
 try {
-  fs.emptyDirSync('build') // Limpa a pasta build se ela existir
-  fs.copySync('src', 'build')
-  console.log('Build concluído com sucesso!')
+  fs.emptyDirSync('build');
+  fs.copySync('src', 'build');
+  console.log('Build concluído com sucesso!');
 } catch (err) {
-  console.error('Erro ao executar o build:', err)
-  process.exit(1) // Força o script a sair com código de erro 1
+  console.error('Erro ao executar o build:', err);
+  process.exit(1); 
 }
